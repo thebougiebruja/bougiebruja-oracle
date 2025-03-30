ef963e626c1ba2da99bdd77dd895ed077837eaf0
thebougiebruja <iamjoportia@gmail.com>
2025-03-30 13:45:47 -0700

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
     const { messages } = await request.json();
 
     const completion = await openai.chat.completions.create({
-      model: 'ft:gpt-3.5-turbo-0613:personal:ai-poet:88GTGA7b',
+      model: 'ft:gpt-3.5-turbo-1106:personal:bougiebruja-oracle:BGZYohMM',
       messages,
       stream: false,
     });
